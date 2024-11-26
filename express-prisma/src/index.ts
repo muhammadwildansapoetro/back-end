37408781c54a13bedbdd360462b14b807339f600
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-11-26 09:59:22 +0700

    update note

---
+++ express-prisma/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const PORT: number = 8000;
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/api", (req: Request, res: Response) => {
+    res.status(200).send("Welcome to my API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/api`);
+});
